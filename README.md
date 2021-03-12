c6388fd11151a91f797f2656825be7eae8dab187
barroudjo <barroudjo@gmail.com>
2021-03-12 15:28:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# lit-html-xsno6v
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/lit-html-xsno6v)
---
+++ src/index.js
@@ -0,0 +1,34 @@
+import { html, svg, render } from "lit-html";
+import { LitElement } from "lit-element";
+import { ScopedElementsMixin } from "@open-wc/scoped-elements";
+
+const renderCircle = () => svg`<circle cx="50" cy="50" r="50"></circle>`;
+
+class TestingSvg extends LitElement {
+  render() {
+    return html`
+      <div>test</div>
+      <svg
+        width="200px"
+        viewBox="0 0 200 200"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        ${renderCircle()}
+      </svg>
+    `;
+  }
+}
+
+window.customElements.define("testing-svg", TestingSvg);
+window.customElements.define(
+  "testing-svg-with-scopedelementmixin",
+  ScopedElementsMixin(TestingSvg)
+);
+render(
+  html`
+    <testing-svg></testing-svg>
+    <testing-svg-with-scopedelementmixin></testing-svg-with-scopedelementmixin>
+  `,
+  document.body
+);
